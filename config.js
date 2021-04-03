b9e04d833c12f606363352d4a798f14a718df65a
Ram Chandra <ramchandra71999@gmail.com>
2021-04-03 13:07:24 +0530

    removed the config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': 'my-secret-key',
-    'mongoUrl': 'mongodb://localhost:27017/confusion',
-    'facebook': {
-        clientId: '571268490727355',
-        clientSecret: '258e5c5a655cb2ec508931dd7e53d202'
-    }
-}
